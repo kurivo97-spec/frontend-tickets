b81b73d93b89dc8559b74162e3efd235aed37de0
KURIVO97 <kurivo97@gmail.com>
2025-10-28 10:13:12 -0500

    Remove invalid comment in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script>
-  import "../app.css"; 
-  // import Navbar from '$lib/components/Navbar.svelte'; // <-- Comentado/Borrado
-  // import { page } from '$app/stores'; // <-- Comentado/Borrado
-  // $: isLoginPage = $page.route.id === '/'; // <-- Comentado/Borrado
+  import "../app.css";
 </script>
 
 <div class="min-h-screen bg-fondo text-texto flex flex-col">
 
-  {/* <-- Comentado/Borrado */}
   <main class="flex-grow">
-    <slot /> 
+    <slot />
   </main>
 
 </div>
