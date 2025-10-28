a97de7210530a251b5cc0ad5249c79ae228cd2c5
KURIVO97 <kurivo97@gmail.com>
2025-10-28 14:31:11 -0500

    Feat: Restore Navbar component in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,22 @@
 <script>
-  import "../app.css"; 
-  import Navbar from '$lib/components/Navbar.svelte'; 
-  import { page } from '$app/stores'; 
-  $: isLoginPage = $page.route.id === '/'; 
+  // Importa el CSS global
+  import "../app.css";
+  // Importa el componente Navbar
+  import Navbar from '$lib/components/Navbar.svelte';
+  // Importa 'page' para saber en qué ruta estamos
+  import { page } from '$app/stores';
+
+  // Variable reactiva: es true si estamos en la página raíz (login)
+  $: isLoginPage = $page.route.id === '/';
 </script>
 
 <div class="min-h-screen bg-fondo text-texto flex flex-col">
+
   {#if !isLoginPage}
-    <Navbar /> 
+    <Navbar />
   {/if}
+
   <main class="flex-grow">
-    <slot /> 
-  </main>
-</div>
+    <slot /> </main>
+
+  </div>
