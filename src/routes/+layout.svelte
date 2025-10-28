db664c192bedcf3af22e2b3dd39fb9e72b7e4c97
KURIVO97 <kurivo97@gmail.com>
2025-10-28 10:29:47 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,8 @@
 <script>
   import "../app.css";
-  import Navbar from '$lib/components/Navbar.svelte'; // Importamos el Navbar
-  import { page } from '$app/stores'; // Para saber la ruta actual
-
-  // Verificamos si la ruta actual es la raíz ('/')
-  $: isLoginPage = $page.route.id === '/';
 </script>
 
 <div class="min-h-screen bg-fondo text-texto flex flex-col">
-
-  {#if !isLoginPage}
-    <Navbar />
-  {/if}
 
   <main class="flex-grow">
     <slot />
