8fc9eddfae0b04d9f31258763116f6db0927d1f7
KURIVO97 <kurivo97@gmail.com>
2025-10-28 11:08:27 -0500

    Fix: Remove invaliDebug: Temporarily disable localStorage read in Navbard comment in layout

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { browser } from '$app/environment';
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  let nombreUsuario = 'Usuario';
+  let idRol = 1; 
+
+  // onMount(() => {
+  //   if (browser) {
+  //     const usuarioData = localStorage.getItem('usuario');
+  //     if (usuarioData) {
+  //       try {
+  //         const usuario = JSON.parse(usuarioData);
+  //         nombreUsuario = usuario.nombre || 'Usuario';
+  //         idRol = usuario.id_rol || 1; 
+  //       } catch (e) {
+  //         console.error("Error al parsear datos de usuario:", e);
+  //       }
+  //     } else {
+  //        handleLogout(); 
+  //     }
+  //   }
+  // }); // <-- Comentado todo el onMount
+
+  function handleLogout() {
+    if (browser) {
+      document.cookie = 'authToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax';
+      localStorage.removeItem('usuario'); // Esto está bien aquí
+      goto('/');
+    }
+  }
+</script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
-  import "../app.css";
+  import "../app.css"; 
+  import Navbar from '$lib/components/Navbar.svelte'; 
+  import { page } from '$app/stores'; 
+  $: isLoginPage = $page.route.id === '/'; 
 </script>
 
 <div class="min-h-screen bg-fondo text-texto flex flex-col">
-
+  {#if !isLoginPage}
+    <Navbar /> 
+  {/if}
   <main class="flex-grow">
-    <slot />
+    <slot /> 
   </main>
-
 </div>
